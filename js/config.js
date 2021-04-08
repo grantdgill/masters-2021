5acf177a6b22cd9007ee7ed8b8d23e1f9b04ea79
Grant Gill <grantdgill@gmail.com>
2021-04-08 08:50:28 -0400

    2021

---
+++ js/config.js
@@ -0,0 +1,236 @@
+var fantasy              = fantasy || {},
+    RORY_MCILROY         = '3470',
+    JORDAN_SPIETH        = '5467',
+    JIMMY_WALKER         = '446',
+    RICKIE_FOWLER        = '3702',
+    JB_HOLMES            = '1067',
+    PHIL_MICKELSON       = '308',
+    BUBBA_WATSON         = '780',
+    DUSTIN_JOHNSON       = '3448',
+    JASON_DAY            = '1680',
+    TIGER_WOODS          = '462',
+    ADAM_SCOTT           = '388',
+    HENRIK_STENSON       = '576',
+    PATRICK_REED         = '5579',
+    MATT_KUCHAR          = '257',
+    BRANDT_SNEDEKER      = '1222',
+    JUSTIN_ROSE          = '569',
+    JASON_DUFNER         = '110',
+    SERGIO_GARCIA        = '158',
+    LEE_WESTWOOD         = '455',
+    LOUIS_OOSTHUIZEN     = '1293',
+    BILLY_HORSCHEL       = '1651',
+    HIDEKI_MATSUYAMA     = '5860',
+    ZACH_JOHNSON         = '686',
+    JIM_FURYK            = '153',
+    CHRIS_KIRK           = '3449',
+    COREY_CONNERS        = '9126',
+    CHARL_SHWARTZEL      = '1097',
+    DANNY_WILLET         = '4304',
+    BROOKS_KOEPKA        = '6798',
+    KEVIN_KISNER         = '2552',
+    JUSTIN_THOMAS        = '4848',
+    BILL_HAAS            = '774',
+    JON_RAHM             = '9780',
+    MARC_LEISHMAN        = '3351',
+    ALEX_NOREN           = '3832',
+    MARTIN_KAYMER        = '3670',
+    GARY_WOODLAND        = '3550',
+    NIEBRUGGE            = '9402',
+    THOMAS_PIETERS       = '9031',
+    BRYSON_DECHAMBEAU    = '10046',
+    FABRIZIO_ZANOTTI     = '1770',
+    TONY_FINAU           = '2230',
+    CHARLEY_HOFFMAN      = '205',
+    PAUL_CASEY           = '72',
+    DANIEL_BERGER        = '9025',
+    KEVIN_CHAPPELL       = '3857',
+    RUSSELL_HENLEY       = '5409',
+    TOMMY_FLEETWOOD      = '5539',
+    IAN_POULTER          = '619',
+    XANDER_SCHAUFFELE    = '10140',
+    KIRADECH_APHIBARNRAT = '5771',
+    FRANCESCO_MOLINARI   = '1483',
+    PATRICK_CANTLAY      = '6007',
+    SUNGJAE_IM           = '11382',
+    COLLIN_MORIKAWA      = '10592',
+    MATTHEW_FITZPATRICK  = '9037',
+    WEBB_SIMPSON         = '1614',
+    MATTHEW_WOLFF        = '4412121',
+    CAMERON_SMITH        = '9131',
+    SHANE_LOWRY          = '4587',
+    CAMERON_CHAMP        = '11098',
+    SCOTTIE_SCHEFFLER    = '9478',
+    MAX_HOMA             = '8973',
+    CARLOS_ORTIZ         = '5532';
+
+
+fantasy.config = {
+    tournamentName = 'Masters -- 2021',
+    tournamentId: 401243010,
+    teams: [
+        {
+            name: 'Jack',
+            golfers: [
+                DUSTIN_JOHNSON,
+                BRYSON_DECHAMBEAU,
+                BROOKS_KOEPKA,
+                COLLIN_MORIKAWA,
+                TONY_FINAU
+            ]
+        },
+        {
+            name: 'Buzz',
+            golfers: [
+                DUSTIN_JOHNSON,
+                BRYSON_DECHAMBEAU,
+                JUSTIN_THOMAS,
+                RORY_MCILROY,
+                PATRICK_CANTLAY
+            ]
+        },
+        {
+            name: 'MattM',
+            golfers: [
+                DUSTIN_JOHNSON,
+                PATRICK_REED,
+                TONY_FINAU,
+                DANIEL_BERGER,
+                WEBB_SIMPSON
+            ]
+        },
+        {
+            name: 'Garrett',
+            golfers: [
+                DUSTIN_JOHNSON,
+                JORDAN_SPIETH,
+                JUSTIN_THOMAS,
+                PATRICK_CANTLAY,
+                XANDER_SCHAUFFELE
+            ]
+        },
+        {
+            name: 'Richard',
+            golfers: [
+                DUSTIN_JOHNSON,
+                JUSTIN_THOMAS,
+                JON_RAHM,
+                XANDER_SCHAUFFELE,
+                COLLIN_MORIKAWA
+            ]
+        },
+        {
+            name: 'Rich',
+            golfers: [
+                DUSTIN_JOHNSON,
+                JUSTIN_THOMAS,
+                JON_RAHM,
+                COLLIN_MORIKAWA,
+                SCOTTIE_SCHEFFLER
+            ]
+        },
+        {
+            name: 'JimW1',
+            golfers: [
+                JORDAN_SPIETH,
+                JUSTIN_THOMAS,
+                XANDER_SCHAUFFELE,
+                PATRICK_REED,
+                MATT_KUCHAR
+            ]
+        },
+        {
+            name: 'JimW2',
+            golfers: [
+                JORDAN_SPIETH,
+                JON_RAHM,
+                RORY_MCILROY,
+                BROOKS_KOEPKA,
+                SERGIO_GARCIA
+            ]
+        },
+        {
+            name: 'MattW',
+            golfers: [
+                DUSTIN_JOHNSON,
+                JON_RAHM,
+                PATRICK_REED,
+                PAUL_CASEY,
+                TOMMY_FLEETWOOD
+            ]
+        },
+        {
+            name: 'Grant',
+            golfers: [
+                DUSTIN_JOHNSON,
+                BRYSON_DECHAMBEAU,
+                JON_RAHM,
+                RORY_MCILROY,
+                SUNGJAE_IM
+            ]
+        },
+        {
+            name: 'Adam',
+            golfers: [
+                DUSTIN_JOHNSON,
+                JORDAN_SPIETH,
+                JON_RAHM,
+                TONY_FINAU,
+                COLLIN_MORIKAWA
+            ]
+        },
+        {
+            name: 'Ted',
+            golfers: [
+                JUSTIN_THOMAS,
+                DUSTIN_JOHNSON,
+                PATRICK_REED,
+                CAMERON_SMITH,
+                PATRICK_CANTLAY
+            ]
+        }, 
+        {
+            name: 'Jetta',
+            golfers: [
+                DUSTIN_JOHNSON,
+                JORDAN_SPIETH,
+                JUSTIN_THOMAS,
+                XANDER_SCHAUFFELE,
+                PATRICK_REED
+            ]
+        },
+        {
+            name: 'John',
+            golfers: [
+                DUSTIN_JOHNSON,
+                BRYSON_DECHAMBEAU,
+                JORDAN_SPIETH,
+                PATRICK_CANTLAY,
+                PATRICK_REED
+            ]
+        }, 
+        {
+            name: 'Dave',
+            golfers: [
+                DUSTIN_JOHNSON,
+                PAUL_CASEY,
+                CAMERON_SMITH,
+                DANIEL_BERGER,
+                MATTHEW_FITZPATRICK
+            ]
+        }, 
+        {
+            name: 'Laura',
+            golfers: [
+                PATRICK_CANTLAY,
+                SUNGJAE_IM,
+                COREY_CONNERS,
+                MAX_HOMA,
+                CARLOS_ORTIZ
+            ]
+        }
+    ],
+    firstPlaceValue: 50,
+    showCut: false,
+    apiKey: '_____'
+};
